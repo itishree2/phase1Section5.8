a841f59681849ca8aae07a4c1cbc45166e12f3c6
itishree2 <nayakitishree6262@gmail.com>
2024-01-04 19:49:23 +0530

    change commited.

---
+++ SubjectTeacher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Phase1Section5._8
+{
+    public class SubjectTeacher : Teacher
+    {
+        private Subject whichSubject;
+
+        public Subject WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+    }
+
+    public class Subject
+    {
+        public string Name { get; internal set; }
+    }
+}
+
+
+
